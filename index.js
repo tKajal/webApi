ba3c59447c9d94b70c8caa4967daad377ba371f3
tKajal <thkkjl@gmail.com>
2023-05-22 09:56:38 +0530

    connection fix

--- index.js
+++ index.js
@@ -1,42 +1,74 @@
+
+var express = require('express');
+var mongoose = require('mongoose');
+var dbUrl = 'mongodb://localhost:27017/demo';
+var bodyParser = require('body-parser')
+mongoose.connect(dbUrl)
+pschema = mongoose.Schema({ name: String, message: String, phone: Number });
+pModel = mongoose.model("pModel", pschema, "test");
+
 const app = require('express')();
 const httpServer = require('http').createServer(app);
-
+const cors = require('cors');
+app.use(cors({
+  origin: '*'
+}));
 const io = require('socket.io')(httpServer, {
   cors: { origin: '*' }
 });
 
-const port =3000;
+const port = 3000;
 const users = {};
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname));
 //listen incoming events
 io.on('connection', function (socket) {
+
+
   socket.on('new-user-joined', (name) => {
 
-   // console.log("new user", name);
+    // console.log("new user", name);
     users[socket.id] = name;
 
     socket.broadcast.emit('user-joined', name)
   })
-  socket.on('send', message => {
-    socket.broadcast.emit('recieve', { message: message, name: users[socket.id] })
+  socket.on('join', (data) => {
+    socket.join(data.room);
+    socket.broadcast.to(data.room).emit('user joined');
+  });
+
+  socket.on('message', (data) => {
+    io.in(data.room).emit('new message', { user: data.user, message: data.message });
+  });
+  // socket.on('send', data => {
+  //   socket.broadcast.emit('recieve', { user: data.user, message: data.message })
+  // })
+
+  socket.on('send', selectedUserRoomId => {
+    socket.broadcast.emit('recieve', { roomId: selectedUserRoomId })
   })
   socket.on('disconnect', message => {
-    socket.broadcast.emit('left', users[socket.id] );
+    socket.broadcast.emit('left', users[socket.id]);
     delete users[socket.id]
   })
 })
 
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
+app.get('/messages', async (req, res) => {
+  let data = await pModel.find(req.phone)
+  res.send(data)
+  console.log(data)
+})
 
-//   socket.on('message', (message) => {
-//     console.log(message);
-//     io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
-//   });
+app.post('/messages', async (req, res) => {
+  var message = new pModel(req.body);
 
-//   socket.on('disconnect', () => {
-//     console.log('a user disconnected!');
-//   });
-// });
+  let result = await message.save();
+  io.emit('message', req.body);
+  if (result) {
+    console.log(result)
+    res.send(result)
+  }
+})
 
 httpServer.listen(port, () => console.log(`listening on port ${port}`));
