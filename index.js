cecd62e9854246f16877f3a5db2c4772cf854fcd
tKajal <thkkjl@gmail.com>
2024-08-28 21:28:33 +0530

    fixing cors issue

--- index.js
+++ index.js
@@ -28,29 +28,25 @@ countData:[{id:String,count:Number}]
 userModel = mongoose.model("userModel", userschema, "users");
 
 const app = require('express')();
+app.use(cors({ origin: '*' }));
 const httpServer = require('http').createServer(app);
-// Define allowed origins
-const allowedOrigins = [
-  'http://localhost:4200',  // Local development URL
-  'https://web-ufuk.vercel.app'  // Production frontend URL
-];
+
 
 // Set up CORS middleware
-const corsOptions = {
-  origin: (origin, callback) => {
-    // Check if the request's origin is in the allowed origins list or if there is no origin (like some server-to-server requests)
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);  // Allow the request
-    } else {
-      callback(new Error('Not allowed by CORS'));  // Reject the request
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,  // Enable to allow cookies and credentials if needed
-  optionsSuccessStatus: 204 // Some legacy browsers choke on 204
-};
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     // Check if the request's origin is in the allowed origins list or if there is no origin (like some server-to-server requests)
+//     if (allowedOrigins.includes(origin) || !origin) {
+//       callback(null, true);  // Allow the request
+//     } else {
+//       callback(new Error('Not allowed by CORS'));  // Reject the request
+//     }
+//   },
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,  // Enable to allow cookies and credentials if needed
+//   optionsSuccessStatus: 204 // Some legacy browsers choke on 204
+// };
 
-app.use(cors(corsOptions));
 
 const io = require('socket.io')(httpServer, {
   cors: { origin: '*' }
