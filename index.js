054b304855bd9837b2d7e5e63f6516a03a883b68
tKajal <thkkjl@gmail.com>
2024-09-06 21:35:07 +0530

    vercel json fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 const cors = require('cors');
-var dbUrl = 'mongodb://localhost:27017/demo';
+var dbUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/demo';
 var bodyParser = require('body-parser')
 const  ObjectID = require('mongodb').ObjectId;
 mongoose.connect(dbUrl)
@@ -59,58 +59,58 @@ io.on('connection', function (socket) {
     // delete users[socket.id]
   })
 })
-app.get('api/messages', async (req, res) => {
-  let data = await pModel.find(req.phone)
-  res.send(data)
-  console.log(data)
-})
-app.post('api/messages', async (req, res) => {
-  console.log(req.body)
+app.get('/messages', async (req, res) => {
+  let data = await pModel.find(req.phone);
+  res.send(data);
+  console.log(data);
+});
+
+app.post('/messages', async (req, res) => {
+  console.log(req.body);
   var message = new pModel(req.body);
   let result = await message.save();
-  //io.emit('message', req.body);
   if (result) {
-    console.log(result)
-    res.send(result)
+    console.log(result);
+    res.send(result);
   }
-})
-app.put('api/messages', async (req, res) => {
-  console.log(req.body)
-  var result = await pModel.updateOne({_id:new  
-    ObjectID(req.body._id)}, { $set: req.body });
-  //io.emit('message', req.body);
-  if (result) {
-    console.log(result)
-    res.send(result)
-  }
-})
-app.put('api/count',async(req,res)=>{
+});
+
+app.put('/messages', async (req, res) => {
+  console.log(req.body);
   var result = await pModel.updateOne(
-    {roomId:req.body.roomId}, { $set: {count:req.body.count} });
-  //io.emit('message', req.body);
+    { _id: new ObjectID(req.body._id) },
+    { $set: req.body }
+  );
   if (result) {
-    console.log(result)
-    res.send(result)
+    console.log(result);
+    res.send(result);
   }
-})
-app.get('api/count/:roomId/:from', async (req, res) => {
-  let data = await pModel.find({roomId:req.params.roomId, from:req.params.from})
-  res.send(data.count)
-  console.log(data)
-})
-app.get('api/users/:id',async(req,res)=>{
-  let data= await userModel.find({user:req.params.id})
-  res.send(data)
-  console.log(data)
-})
-app.post('api/users', async (req, res) => {
-  console.log(req.body)
+});
+
+app.get('/count/:roomId/:from', async (req, res) => {
+  let data = await pModel.find({ roomId: req.params.roomId, from: req.params.from });
+  res.send(data.count);
+  console.log(data);
+});
+
+app.get('/users/:id', async (req, res) => {
+  let data = await userModel.find({ user: req.params.id });
+  res.send(data);
+  console.log(data);
+});
+
+app.get('/test', async (req, res) => {
+  res.send({ "test": 'iuri' });
+  res.end();
+});
+
+app.post('/users', async (req, res) => {
+  console.log(req.body);
   var data = new userModel(req.body);
   let result = await data.save();
-  //io.emit('message', req.body);
   if (result) {
-    console.log(result)
-    res.send(result)
+    console.log(result);
+    res.send(result);
   }
-})
+});
 httpServer.listen(port, () => console.log(`listening on port ${port}`));
